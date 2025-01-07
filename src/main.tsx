d50c5efa7c2dd5f16311d03736d8b48c2c9d0122
Dmitrii Bulatov <bulati484@gmail.com>
2025-01-07 20:56:15 +0300

    corr

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('/service-worker.ts')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
